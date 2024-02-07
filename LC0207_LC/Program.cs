ccb3fea38cfaf1b8e644ecff84400cf65b71a3ac
Alexandra Camarena <alexandracamarena@ou.edu>
2024-02-07 13:21:12 -0600

    Add project files.

---
+++ LC0207_LC/Program.cs
@@ -0,0 +1,51 @@
+﻿// MIS 3013 001
+// Feb 7, 2024
+// Alexandra Camarena
+// 113568154
+
+Console.WriteLine("if else");// conditional statement
+// int double bool char
+double grade;// grade,double, simple, not expensive resources
+// double room card,
+// positive 1
+grade = 80.6 + 1;//assignment 1. CPU, 81.6 2. break point
+
+// format 1 
+if (grade >= 90) //; here is wrong
+{// block 1
+    Console.WriteLine("Congrats!");
+    Console.WriteLine("A");
+}
+else if (grade >= 80)// no;
+{ // block 2
+    Console.WriteLine("Sorry!");
+    Console.WriteLine("NOT A");
+}
+else if (grade >= 70)// no;
+{ // block 3
+    Console.WriteLine("Congrats!");
+    Console.WriteLine("C");
+}
+else if (grade >= 60)// no;
+{// block 4
+    Console.WriteLine("You need to work hard!");
+    Console.WriteLine("D");
+}
+else // no;
+{ // block 5
+    Console.WriteLine("Sorry!");
+    Console.WriteLine("F");
+}
+
+// ;
+Console.WriteLine("This is the next line of if else");
+
+// counter, accumulator
+double bal;// double, simple, not expensive resources
+bal = 100;
+
+bal = bal + 20;
+
+bal = bal - 5;
+
+Console.WriteLine(bal);
